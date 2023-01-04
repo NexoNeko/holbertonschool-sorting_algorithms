f026c4d35bc3868bde763da5220990b5538cb3ff
NexoNeko <meresidamente@hotmail.com>
2023-01-04 07:49:36 -0800

    Deleted aux functions, merged function to insert function

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -32,3 +32,22 @@ void insertion_sort_list(listint_t **list)
 	while ((*list)->prev)
 		*list = (*list)->prev;
 }
+
+/**
+* swap_nodes - swaps two nodes in a doubly linked list
+*
+* @to_right - node on the left that's going right| R | >> | L |
+* @to_left - node on the right that's going left | R | << | L |
+*/
+void swap_nodes(listint_t *to_right, listint_t *to_left)
+{
+	if (to_left->next)
+		to_left->next->prev = to_right;
+	if (to_right->prev)
+		to_right->prev->next = to_left;
+
+	to_right->next = to_left->next;
+	to_left->next = to_right;
+	to_left->prev = to_right->prev;
+	to_right->prev = to_left;
+}
