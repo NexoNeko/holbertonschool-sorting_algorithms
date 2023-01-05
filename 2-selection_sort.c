a2cc3fc74d08b3ce6a06e73a9cbc26e6e466bc40
Elias Martin <5715@holbertonstudents.com>
2023-01-05 05:37:03 -0800

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,50 +10,24 @@ void selection_sort(int *array, size_t size)
 size_t it = 0, swap;
 
 
-for(swap = 0; swap < size; swap++)
+for (swap = 0; swap < size; swap++)
 {
-minimo = array[swap];
+	minimo = array[swap];
 
-	for(it = swap + 1; it < size; it++)
+	for (it = swap + 1; it < size; it++)
 	{
-			if(array[it] < minimo)
-			{
-				minimo = array[it];
-				index = it;
-			}
-	}	
-	if(swap != size - 1)
+		if (array[it] < minimo)
+		{
+		minimo = array[it];
+		index = it;
+		}
+	}
+	if (array[swap] != minimo)
 	{
 	temporal = array[swap];
 	array[swap] = minimo;
 	array[index] = temporal;
 	print_array(array, size);
 	}
-	
 }
 }
-
-
-	/**
-	int hand;
-	size_t i, j = 0;
-
-for (; j < size; j++)
-{
-	for (i = j + 1; i < size; i++)
-	{
-		if (array[j] < array[i])
-		{
-		}
-		else
-		{
-			hand = array[j];
-			array[j] = array[i];
-			array[i] = hand;	
-		}
-	}
-	
-	print_array(array, size);
-}
-}
-**/
